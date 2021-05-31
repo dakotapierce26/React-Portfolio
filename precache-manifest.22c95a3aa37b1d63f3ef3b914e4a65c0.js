9c91b85b172d678d2689e62a13a367adc029142e
Dakota <piercedakota26@gmail.com>
2021-05-30 21:08:58 -0500

    Updates

---
+++ precache-manifest.22c95a3aa37b1d63f3ef3b914e4a65c0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b18ea5ed73303da578de1d41bdc85833",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "e07025f21ab8bf7e2a9a",
+    "url": "/react-portfolio/static/css/main.0d07ca56.chunk.css"
+  },
+  {
+    "revision": "a0815d22085ef4750b5b",
+    "url": "/react-portfolio/static/js/2.ca3d2884.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/react-portfolio/static/js/2.ca3d2884.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e07025f21ab8bf7e2a9a",
+    "url": "/react-portfolio/static/js/main.55db9f48.chunk.js"
+  },
+  {
+    "revision": "aff33cef9dbb8787d6b3",
+    "url": "/react-portfolio/static/js/runtime-main.213b1880.js"
+  }
+]);
